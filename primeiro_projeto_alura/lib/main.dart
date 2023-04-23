0215be49337a8153efbe9027a4d5452056e98829
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-04-23 13:27:02 -0300

    starting of the firt project  on the flutter

--- primeiro_projeto_alura/lib/main.dart
+++ primeiro_projeto_alura/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,78 +15,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Container(
-            color: Colors.white,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      Container(
-                        color: Colors.red,
-                        width: 100,
-                        height: 100,
-                      ),
-                      Container(
-                        color: Colors.blue,
-                        width: 50,
-                        height: 50,
-                      )
-                    ],
-                  ),
-                  Stack(alignment: AlignmentDirectional.center, children: [
-                    Container(
-                      color: Colors.blue,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.red,
-                      width: 50,
-                      height: 50,
-                    )
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        color: Colors.cyan,
-                        width: 50,
-                        height: 50,
-                      ),
-                      Container(
-                        color: Colors.pinkAccent,
-                        width: 50,
-                        height: 50,
-                      ),
-                      Container(
-                        color: Colors.purple,
-                        width: 50,
-                        height: 50,
-                      )
-                    ],
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    height: 30,
-                    width: 300,
-                    child: Text(
-                      'Diamante Amarelo',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 28,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('Você apertou o botão');
-                      },
-                      child: Text('Aperte o botão!!'))
-                ])));
+        home: Scaffold(
+          appBar: AppBar( title: Text('Flutter: meus primeiros passos'),),
+        body: Container(),
+        floatingActionButton: FloatingActionButton(onPressed: (){}),
+        ),
+         );
   }
 }
