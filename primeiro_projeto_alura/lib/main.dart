64c96c2ff58c5857714264489c46f1e2256c1a90
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-04-29 12:30:13 -0300

    Update main.dart

--- primeiro_projeto_alura/lib/main.dart
+++ primeiro_projeto_alura/lib/main.dart
@@ -11,15 +11,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
         ),
-        home: Scaffold(
-          appBar: AppBar( title: Text('Flutter: meus primeiros passos'),),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(onPressed: (){}),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text("Aprender Flutter"),
+                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-         );
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
   }
 }
