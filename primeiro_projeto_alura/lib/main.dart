2ff93e83020fa1cc6c9c23e666f5c69eac0227a6
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-04-30 15:06:39 -0300

    update

--- primeiro_projeto_alura/lib/main.dart
+++ primeiro_projeto_alura/lib/main.dart
@@ -16,37 +16,110 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tarefas'),
-        ),
-        body: Container(
+          appBar: AppBar(
+            leading: Container(),
+            title: Text('Tarefas'),
+          ),
+          body: ListView(children: [
+            Task('Aprender Flutter tomando café da manhã'),
+            Task('Andar de bike'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+          ])),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String nome;
+  const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
           child: Stack(
             children: [
               Container(
                 color: Colors.blue,
                 height: 140,
               ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Text("Aprender Flutter"),
-                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-                  ],
+              Column(children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black26,
+                          width: 72,
+                          height: 100,
+                        ),
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 52,
+                          width: 52,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  level++;
+                                });
+                                print(level);
+                              },
+                              child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Icon(Icons.arrow_drop_up),
+                                    Text('up')
+                                  ])),
+                        ),
+                      ]),
                 ),
-              )
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                          child: LinearProgressIndicator(
+                            color: Colors.white,
+                            value: level / 10,
+                          ),
+                          width: 200,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          'Nivel: $level',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ])
+              ])
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
+        ));
   }
 }
