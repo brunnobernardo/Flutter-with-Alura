9b1fb50166c14abb6544f6cd9d3b0446f09fe69b
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-05-06 11:46:55 -0300

    Created ex002 (home page  of app control workload)

--- flutter_ex002/lib/main.dart
+++ flutter_ex002/lib/main.dart
@@ -11,46 +11,140 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Container(
-            child: Icon(Icons.account_circle),
+          appBar: AppBar(
+            leading: Container(
+              child: Icon(Icons.account_circle),
+            ),
+            title: Text('Controle de ponto'),
           ),
-          title: Text('Flutter: Primeiros Passos'),
-        ),
-        body: mainContainer(),// body
-      ),
+          body: ListView(children: [
+            mainContainer('08/05', 'Monday'),
+            mainContainer('09/05', 'Tuesday'),
+            mainContainer('10/05', 'Wednesday'),
+            mainContainer('11/05', 'Thursday'),
+            mainContainer('12/05', 'Friday')
+          ])
+          // body
+          ),
     );
   }
 }
 
 class mainContainer extends StatefulWidget {
-  const mainContainer ({Key? key}) : super(key: key);
+  final String date;
+  final String day;
+  const mainContainer(this.date, this.day, {Key? key}) : super(key: key);
 
   @override
   State<mainContainer> createState() => _MyWidgetState();
 }
 
 class _MyWidgetState extends State<mainContainer> {
+  int hours = 0;
+  int workload = 10;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child:
-    Stack(
-      children: [
-        Container(
-          height: 80,
-          color: Colors.grey,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.only(top: 20),
+        child: Stack(
           children: [
             Container(
-              child: ,
-            )
+              height: 100,
+              color: Colors.grey,
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: AlignmentDirectional.center,
+                        width: 100,
+                        height: 50,
+                        child: Text(
+                          widget.date,
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      Container(
+                        alignment: AlignmentDirectional.center,
+                        width: 150,
+                        height: 25,
+                        child: Text(
+                          widget.day,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: AlignmentDirectional.center,
+                        width: 155,
+                        height: 50,
+                        child: Text(
+                          'Trabalhando: ${hours}h',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      Container(
+                        alignment: AlignmentDirectional.center,
+                        width: 150,
+                        height: 25,
+                        child: Text(
+                          'Saldo: ${hours - workload}h ',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        Container(
+                          height: 30,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  hours++;
+                                });
+                                print(hours);
+                              },
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '+',
+                                      style: TextStyle(fontSize: 20),
+                                    )
+                                  ])),
+                        ),
+                        Container(
+                          height: 30,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  hours--;
+                                });
+                                print(hours);
+                              },
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '-',
+                                      style: TextStyle(fontSize: 20),
+                                    )
+                                  ])),
+                        )
+                      ]))
+                ],
+              ),
+            ),
           ],
-        )
-      ],
-    ));
+        ));
   }
 }
