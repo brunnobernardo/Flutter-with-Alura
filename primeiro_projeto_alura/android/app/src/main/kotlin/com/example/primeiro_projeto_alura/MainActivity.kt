a4d7e77e7a1008117a0aa3a7599962db3e01101c
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-06-18 14:04:05 -0300

    update

--- primeiro_projeto_alura/android/app/src/main/kotlin/com/example/primeiro_projeto_alura/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.primeiro_projeto_alura
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projeto_alura/android/app/src/main/kotlin/com/example/projeto_alura/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projeto_alura
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
